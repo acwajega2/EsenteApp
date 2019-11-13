7acb45ca1e7b3574f6040051e7367890f5c3970a
acwajega@wacsoftsolutions.com <acwajega@gmail.com>
2019-11-13 17:59:58 +0300

    second commit

--- EssenteApp/EssenteApp/settings.py
+++ EssenteApp/EssenteApp/settings.py
@@ -74,9 +74,14 @@ WSGI_APPLICATION = 'EssenteApp.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'NAME': 'EssenteApp_DB',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'root',
+        'PASSWORD': 'matilda',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
 
